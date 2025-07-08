9c510788e3c33559e16a940933eb065a4fb7ec6c
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-07-08 21:06:36 +0700

    chore: project setup

---
+++ src/main/java/com/personal/spring_questly/SpringQuestlyApplication.java
@@ -0,0 +1,13 @@
+package com.personal.spring_questly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuestlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuestlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/spring_questly/SpringQuestlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.spring_questly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuestlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
