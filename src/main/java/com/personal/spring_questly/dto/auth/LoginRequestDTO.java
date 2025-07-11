1c2a6eb83faabb854aaa4307b9817d58b46f02c1
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-07-11 21:22:29 +0700

    docs: add description at the auth dtos

--- src/main/java/com/personal/spring_questly/dto/auth/LoginRequestDTO.java
+++ src/main/java/com/personal/spring_questly/dto/auth/LoginRequestDTO.java
@@ -1,5 +1,6 @@
 package com.personal.spring_questly.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
@@ -8,9 +9,11 @@ import lombok.Builder;
 public record LoginRequestDTO(
         @NotBlank
         @Email
+        @Schema(description = "Email", example = "test123@gmail.com")
         String email,
 
         @NotBlank
+        @Schema(description = "Password")
         String password
 ) {
 }
--- src/main/java/com/personal/spring_questly/dto/auth/RegisterRequestDTO.java
+++ src/main/java/com/personal/spring_questly/dto/auth/RegisterRequestDTO.java
@@ -1,5 +1,6 @@
 package com.personal.spring_questly.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,20 +11,28 @@ import org.hibernate.validator.constraints.Length;
 public record RegisterRequestDTO(
         @NotBlank
         @Email
+        @Schema(description = "Email", example = "test123@gmail.com")
         String email,
 
         @NotBlank
+        @Schema(description = "Password")
         String password,
 
         @Length(min = 3)
+        @Schema(description = "First name user. Example: 'John'", example = "John")
         String firstName,
 
         @Length(min = 3)
+        @Schema(description = "Last name user. Example: 'Doe'", example = "Doe")
         String lastName,
 
         @NotNull
+        @Schema(description = "Indicates whether the user wants to be a quester",
+                example = "true")
         Boolean isQuester,
 
+        @Schema(description = "Indicates whether the user wants to be a requester",
+                example = "true", nullable = true)
         boolean isRequester
 ) {
 }
