243cfe1e61367450ce09f9843baefcf0fd9affa8
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-07-11 17:58:25 +0700

    add: argument not valid exception handler at global exception advice

--- src/main/java/com/personal/spring_questly/advice/GlobalExceptionHandlerAdvice.java
+++ src/main/java/com/personal/spring_questly/advice/GlobalExceptionHandlerAdvice.java
@@ -6,8 +6,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -28,6 +33,30 @@ public class GlobalExceptionHandlerAdvice {
                                                         .build();
 
         return ResponseEntity.status(ex.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDTO<Object>> handlerArgumentNotValidException(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest req
+    ) {
+        log.info("Method argument not valid error at: {}", req.getRequestURL());
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : ex.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ApiResponseDTO<Object> response = ApiResponseDTO.builder()
+                                                        .status(status.value())
+                                                        .message("Some values are invalid")
+                                                        .data(errors)
+                                                        .build();
+
+        return ResponseEntity.status(status).body(response);
+
     }
 
     @ExceptionHandler(Exception.class)
