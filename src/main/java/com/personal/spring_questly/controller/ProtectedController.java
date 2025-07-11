15c9cada7c346362586eea1f8c5229c5a937cece
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-07-11 21:51:02 +0700

    add: protected route for test purpose

---
+++ src/main/java/com/personal/spring_questly/controller/ProtectedController.java
@@ -0,0 +1,67 @@
+package com.personal.spring_questly.controller;
+
+import com.personal.spring_questly.annotation.auth.QuesterOnly;
+import com.personal.spring_questly.annotation.auth.RequesterOnly;
+import com.personal.spring_questly.dto.common.ApiResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/protected")
+public class ProtectedController {
+
+    @GetMapping(value = "quester")
+    @QuesterOnly
+    public ResponseEntity<ApiResponseDTO<Object>> getOnlyQuesterRoute() {
+        ApiResponseDTO<Object> response = ApiResponseDTO.builder()
+                                                        .status(HttpStatus.OK.value())
+                                                        .message("Yes this is quester only route")
+                                                        .data(this.getLoggedUserData())
+                                                        .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "requester")
+    @RequesterOnly
+    public ResponseEntity<ApiResponseDTO<Object>> getOnlyRequesterRoute() {
+        ApiResponseDTO<Object> response = ApiResponseDTO.builder()
+                                                        .status(HttpStatus.OK.value())
+                                                        .message("Yes this is requester" +
+                                                                " only route")
+                                                        .data(this.getLoggedUserData())
+                                                        .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    private UserDetails getLoggedUserDetails() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        return (UserDetails) authentication.getPrincipal();
+    }
+
+    private Object getLoggedUserData() {
+        UserDetails userDetails = this.getLoggedUserDetails();
+
+        Map<String, Object> loggedUserData = new HashMap<>();
+
+        loggedUserData.put("email", userDetails.getUsername());
+        loggedUserData.put("authorities",
+                userDetails.getAuthorities().stream().map(Object::toString).toList());
+
+        return loggedUserData;
+    }
+
+}
